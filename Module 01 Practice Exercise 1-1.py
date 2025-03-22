67e53825e96d0280e6fb419b7cff23e089d9e174
you-love-lucy <fjones96@ivytech.edu>
2025-03-22 16:25:07 -0400

    Add files via upload

---
+++ Module 01 Practice Exercise 1-1.py
@@ -0,0 +1,12 @@
+"""
+Author:  Lucy Jones
+Date written: 03/22/25
+Assignment:   Module 01 Practice Exercise 1-1
+Short Desc:   This program takes input for name, phone number, and input and prints them.
+"""
+
+userName = input("Enter name: ")
+userNum = input("Enter phone number: ")
+userAddress = input("Enter address: ")
+
+print(userName, userNum, userAddress)
